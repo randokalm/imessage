ea831b69f58213a013a007e7b2b1f331b6ca2723
randokalm <erdalaskin716@gmail.com>
2026-07-03 21:41:05 +0300

    fix import path for User model in clerk.webhook.js

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/user.model.js";
+import User from "../models/User.js";
 import { verifyWebhook } from "@clerk/backend/webhooks";
 
 const router = express.Router();
