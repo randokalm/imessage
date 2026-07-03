2a5599dba7f973b47d6133cd97dc08ba6936b6ac
randokalm <erdalaskin716@gmail.com>
2026-07-03 15:07:19 +0300

    add Dockerfile and .dockerignore; implement static file serving in Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cors from "cors"
+
 import "dotenv/config"
+
+import fs from "fs";
+import path from "path";
 
 import { clerkMiddleware } from '@clerk/express'
 
@@ -11,6 +15,8 @@ const app = express()
 const PORT = process.env.PORT 
 const FRONTEND_URL = process.env.FRONTEND_URL; 
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware());
@@ -18,6 +24,16 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 });
+
+//if the public directory exists, serve the static files from it
+//this is for the production build of the frontend
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+  
+    app.get("/{*any}", (req, res, next) => {
+      res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+  }  
 
 app.listen(PORT,() =>{
     connectDB();
