e9524af09201e281cb40527fb07bc3ba24cf75fb
randokalm <erdalaskin716@gmail.com>
2026-07-03 21:33:50 +0300

    add Clerk webhook integration and update index.js for proper middleware usage

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors"
-import job from "./lib/cron.js";
 
 import "dotenv/config"
 
@@ -11,12 +10,19 @@ import { clerkMiddleware } from '@clerk/express'
 
 import User from "./models/User.js"
 import { connectDB } from "./lib/db.js"
+import job from "./lib/cron.js";
+
+import clerckWebhook from ".webhooks/clerk.webhook.js";
 
 const app = express()
 const PORT = process.env.PORT 
 const FRONTEND_URL = process.env.FRONTEND_URL; 
 
 const publicDir = path.join(process.cwd(), "public");
+
+
+//it is important to import the webhook route after the clerkMiddleware, otherwise the webhook will not work
+app.use("/api/webhooks/clerk",express.raw({type:"applicatipon/json"}),clerckWebhook);
 
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
